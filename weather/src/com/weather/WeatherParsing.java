a351c48307993279aa477cc6e80289b17741e8a0
kymkyj <ymyj33@naver.com>
2019-04-16 14:14:47 +0900

    WeatherPasing, Util 불필요한 주석 제거 및 중복코드 따로 빼서 정리

--- weather/src/com/weather/WeatherParsing.java
+++ weather/src/com/weather/WeatherParsing.java
@@ -1,141 +1,83 @@
 package com.weather;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 public class WeatherParsing {
-    public static void main(String[] args) {
-    	System.out.println("## 프로그램 시작 ##");
-    	
-        double gangSu = 0.0;
-        
-        // 일기예보 확인
-        final String CATEGORY_POP = "POP"; // 강수량만 필요함으로 final 변수로 선언
+	public static void main(String[] args) {
+		System.out.println("## 프로그램 시작 ##");
 
-        // 조회를 원하는 지역의 경도 위도 - 서울 중구 기준 60 127
-        String nx = "60"; // 경도
-        String ny = "127"; // 위도
+		double gangSu = 0.0;
 
-        // 현재 날짜 가져오는 힘수
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd"); // 날짜 데이터 포멧 형태지정
-        String todayDate = sdf.format(cal.getTime());
+		// 일기예보 확인
+		final String CATEGORY_POP = "POP";
 
-        String alamTime = "0800"; // 조회하고 싶은 시간대 지정
-        // 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회 제한)
-        
-        String serviceKey = "gjacaWQfv4ZtfFSgJrq5FaCGNJrQCVWaila1%2F%2BY66oOEUkrrmMh7YgG57jCakEhXN1C1PA2r5zmISvqkZYqq%2FQ%3D%3D";
-        // 공공데이터 포털에서 제공해준 서비스 키
-        
-        // 정보를 모아서 URL정보를 만들면됩니다. 맨 마지막 "&_type=json"에 따라 반환 데이터의 형태가 정해집니다.
-        String gangsuUrl = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?"
-                + "serviceKey=" + serviceKey + "&base_date=" + todayDate + "&base_time=" + alamTime + "&nx=" + nx
-                + "&ny=" + ny + "&_type=json";
-        
-        try {
-            URL url = new URL(gangsuUrl); // 위 urlStr을 이용해서 URL 객체를 만들어줍니다.
-            BufferedReader br;
-            String line = "";
-            String result = "";
+		// 서울 중구 기준 60 127
+		String nx = "60"; // 경도
+		String ny = "127"; // 위도
 
-            br = new BufferedReader(new InputStreamReader(url.openStream()));
+		// 현재 날짜 가져오는 힘수
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd"); 
+		String todayDate = sdf.format(cal.getTime());
 
-            while ((line = br.readLine()) != null) {
-                result = result.concat(line);
-            }
+		String alamTime = "0800"; // 조회하고 싶은 시간대 지정
+		// 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회 제한)
 
-            // Json parser를 만들어 만들어진 문자열 데이터를 객체화 합니다.
-            JSONParser parser = new JSONParser();
+		String serviceKey = "gjacaWQfv4ZtfFSgJrq5FaCGNJrQCVWaila1%2F%2BY66oOEUkrrmMh7YgG57jCakEhXN1C1PA2r5zmISvqkZYqq%2FQ%3D%3D";
+		
+		String gangsuUrl = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?"
+				+ "serviceKey=" + serviceKey + "&base_date=" + todayDate + "&base_time=" + alamTime + "&nx=" + nx
+				+ "&ny=" + ny + "&_type=json";
 
-            JSONObject obj = (JSONObject) parser.parse(result);
-            
-            // Top레벨 단계인 response 키를 가지고 데이터를 파싱합니다.
-            JSONObject wt_response = (JSONObject) obj.get("response");
-            // response 로 부터 body 찾아옵니다.
-            JSONObject wt_body = (JSONObject) wt_response.get("body");
-            // body 로 부터 items 받아옵니다.
-            JSONObject wt_items = (JSONObject) wt_body.get("items");
+		JSONObject obj = WeatherUtil.getJsonObjectByURL(gangsuUrl);
 
-            // items로 부터 itemlist 를 받아오기 itemlist : 뒤에 [ 로 시작하므로 jsonarray이다
-            JSONArray wt_item = (JSONArray) wt_items.get("item");
+		JSONObject wt_response = (JSONObject) obj.get("response");
+		JSONObject wt_body = (JSONObject) wt_response.get("body");
+		JSONObject wt_items = (JSONObject) wt_body.get("items");
+		JSONArray wt_item = (JSONArray) wt_items.get("item");
 
-            String category; // 강수량인 POP만 꺼내오려고 사용
-            JSONObject weather; // 배열 형태의 구조에서 하나씩 데이터를 꺼내올때 담을 변수
+		String category; 
+		JSONObject weather;
 
-            for (int i = 0; i < wt_item.size(); i++) {
-                weather = (JSONObject) wt_item.get(i);
-                double value = (Double.parseDouble(weather.get("fcstValue").toString())); // 실수로된 값과 정수로된 값이 둘다 있어서 실수로
+		for (int i = 0; i < wt_item.size(); i++) {
+			weather = (JSONObject) wt_item.get(i);
+			double value = (Double.parseDouble(weather.get("fcstValue").toString()));
 
-                category = (String) weather.get("category");
+			category = (String) weather.get("category");
 
-                if (category.equals(CATEGORY_POP)) {
-                    gangSu = value; // 강수량 데이터만 가져옴
-                }
-            }
-            String rs = WeatherUtil.sendTelegram(WeatherUtil.makeMsg(gangSu));
-            System.out.println("## 일기예보 종료 ##");
+			if (category.equals(CATEGORY_POP)) {
+				gangSu = value;
+			}
+		}
+		
+		
+		// 미세먼지 체크
+		String sido = "서울";
 
-            br.close();
+		try {
+			sido = URLEncoder.encode(sido, "UTF8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        
-        
-        // 미세먼지 체크
-        
-        String sido = "서울";
-        
-        try {
-            sido = URLEncoder.encode(sido, "UTF8");    
+		String urlMise = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName="
+				+ sido + "&pageNo=1&numOfRows=10&ServiceKey=" + serviceKey + "&ver=1.3&_returnType=json";
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        String urlMise = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=" + sido + "&pageNo=1&numOfRows=10&ServiceKey=" + serviceKey + "&ver=1.3&_returnType=json";
-        
-        try {
-            URL url = new URL(urlMise); // 위 urlStr을 이용해서 URL 객체를 만들어줍니다.
-            BufferedReader br;
-            String line = "";
-            String result = "";
+		JSONObject miseObj = WeatherUtil.getJsonObjectByURL(urlMise);
 
-            br = new BufferedReader(new InputStreamReader(url.openStream()));
+		JSONArray item = (JSONArray) miseObj.get("list");
+		JSONObject miseDosi = (JSONObject) item.get(0);
 
-            while ((line = br.readLine()) != null) {
-                result = result.concat(line);
-            }
-            
-            JSONParser parser = new JSONParser();
-            JSONObject obj = (JSONObject) parser.parse(result);
-            
-            JSONArray item = (JSONArray) obj.get("list");
-            System.out.println("item :"+item);
-            JSONObject miseDosi = (JSONObject) item.get(0);
-            // 공공데이터 포탈에서 던져주는 데이터가 배열형태로 도시대기인 0번째 값을 기준으로 함
-            
-            int miseGrade = Integer.parseInt((String)miseDosi.get("pm10Grade")); // 미세먼지 등급
-            double miseValue = Double.parseDouble((String)miseDosi.get("pm10Value")); // 미세먼지 수치
-            
-            String rs = WeatherUtil.sendTelegram(WeatherUtil.makeMsg(miseGrade, miseValue));
-            System.out.println("## 미세먼지 종료 ##");
+		int miseGrade = Integer.parseInt((String) miseDosi.get("pm10Grade")); 
+		double miseValue = Double.parseDouble((String) miseDosi.get("pm10Value"));
 
-            br.close();
+		String rs = WeatherUtil.sendTelegram(WeatherUtil.makeMsg(gangSu, miseGrade, miseValue));
 
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-       
-    }
-
+		System.out.println("## 종료 ##");
+	}
 }
--- weather/src/com/weather/WeatherUtil.java
+++ weather/src/com/weather/WeatherUtil.java
@@ -6,7 +6,38 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class WeatherUtil {
+	static JSONObject getJsonObjectByURL(String urlStr) {
+		try {
+			URL url = new URL(urlStr);
+			BufferedReader br;
+			String line = "";
+			String result = "";
+			
+			br = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			while((line = br.readLine()) != null) {
+				result = result.concat(line);
+			}
+			
+			JSONParser parser = new JSONParser();
+			
+			JSONObject obj = (JSONObject) parser.parse(result);
+			br.close();
+			
+			return obj;
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+			return null;
+		}
+		
+	}
+	
     static String sendTelegram(String msg){
         try {
             String telegramUrl = "https://api.telegram.org/bot785529566:AAGJgIYz_uzbcogAVrrUEBjkq9gdxXF1JzU/sendmessage?chat_id=639995102&text=";
@@ -33,26 +64,20 @@ public class WeatherUtil {
         }
     }
 
-    static String makeMsg(double gangSu){
-        String result = ""; // 조건에 따라 던져줄 메시지를 넣을 변수
+    static String makeMsg(double gangSu, int miseGrade, double miseValue){
+        String result = ""; 
         result += "오늘의 강수확률은 " + gangSu + "% 입니다. \n";
 
-        if(gangSu >= 40.0){ // 강수량이 40.0 이상일 경우 아래 내용 수행
+        if(gangSu >= 40.0){ 
             result += "우산을 준비하세요! \n";
         }
-        return result;
-    }
-    
-    static String makeMsg(int miseGrade, double miseValue){
-        String result = ""; // 조건에 따라 던져줄 메시지를 넣을 변수
         
         result += "오늘의 미세먼지 수치는 " + miseValue + "입니다. \n";
         
-        if(miseGrade >= 3) { // 미세먼지 등급이 3이상인 경우 나쁨이므로 마스크 준비
+        if(miseGrade >= 3) { 
         	result += "미세먼지가 나쁘니, 마스크를 준비하세요!";
         }
-
+        
         return result;
     }
-
 }
