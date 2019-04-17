80aab7e4562b4c02a67b21a992f7833cf0dde7ab
kymkyj <ymyj33@naver.com>
2019-04-17 09:49:10 +0900

    WeatherUtil에 String -> StringBuilder 사용해서 코드 수정

--- weather/src/com/weather/WeatherUtil.java
+++ weather/src/com/weather/WeatherUtil.java
@@ -17,59 +17,59 @@ public class WeatherUtil {
 			BufferedReader br;
 			String line = "";
 			String result = "";
-			
+
 			br = new BufferedReader(new InputStreamReader(url.openStream()));
-			
-			while((line = br.readLine()) != null) {
+
+			while ((line = br.readLine()) != null) {
 				result = result.concat(line);
 			}
-			
+
 			JSONParser parser = new JSONParser();
-			
+
 			JSONObject obj = (JSONObject) parser.parse(result);
 			br.close();
-			
+
 			return obj;
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 			return null;
 		}
-		
+
 	}
-	
-    static void sendTelegram(String msg){
-        try {
-            String telegramUrl = "https://api.telegram.org/bot785529566:AAGJgIYz_uzbcogAVrrUEBjkq9gdxXF1JzU/sendmessage?chat_id=639995102&text=";
 
-            URL url = new URL(telegramUrl + URLEncoder.encode(msg, "UTF8"));
+	static void sendTelegram(String msg) {
+		try {
+			String telegramUrl = "https://api.telegram.org/bot785529566:AAGJgIYz_uzbcogAVrrUEBjkq9gdxXF1JzU/sendmessage?chat_id=639995102&text=";
 
-            HttpsURLConnection conn =  (HttpsURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			URL url = new URL(telegramUrl + URLEncoder.encode(msg, "UTF8"));
 
-            br.close();
-            
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-    static String makeMsg(double gangSu, int miseGrade, double miseValue){
-        String result = ""; 
-        result += "오늘의 강수확률은 " + gangSu + "% 입니다. \n";
+			br.close();
 
-        if(gangSu >= 40.0){ 
-            result += "우산을 준비하세요! \n";
-        }
-        
-        result += "오늘의 미세먼지 수치는 " + miseValue + " 이고, 등급은 " + miseGrade + " 입니다. \n" + "ex) 미세먼지 등급 \n" + "[1-매우좋음, 2-좋음, 3-나쁨, 4-매우나쁨] \n";
-        
-        if(miseGrade >= 3) { 
-        	result += "미세먼지가 나쁘니, 마스크를 준비하세요!";
-        }
-        
-        return result;
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static String makeMsg(double gangSu, int miseGrade, double miseValue) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("오늘의 강수확률은 " + gangSu + "% 입니다. \n");
+
+		if (gangSu >= 40.0) {
+			sb.append("우산을 준비하세요! \n");
+		}
+
+		sb.append("오늘의 미세먼지 수치는 " + miseValue + " 이고, 등급은 " + miseGrade + " 입니다. \n" + "ex) 미세먼지 등급 \n" + "[1-매우좋음, 2-좋음, 3-나쁨, 4-매우나쁨] \n");
+
+		if (miseGrade >= 3) {
+			sb.append("미세먼지가 나쁘니, 마스크를 준비하세요!");
+		}
+
+		return sb.toString();
+	}
 }
